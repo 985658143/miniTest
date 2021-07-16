e4e36c836e7adca6720d21ed06b9085788193e7e
luozf <luozf@qingcigame.com>
2021-07-16 13:49:37 +0800

    08网络请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,3 +1,5 @@
+import request from '../../service/network'
+
 Page({
 
   /**
@@ -6,60 +8,51 @@ Page({
   data: {
     
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-    
-  },
+    //------------1.原生的方式发送网络请求-----------
+    this.get_data_origin()
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
+    //------------2.使用封装的request发送网络请求
+    request({
+      url: 'https://api.qingcigame.com/web/lantern/pet',
+    })
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  get_data_origin() {
+    // 发送网络请求
+    //1.发送最简单的get请求
+    // wx.request({
+    //   url: 'https://api.qingcigame.com/web/lantern/pet',
+    //   success: function (res) {
+    //     console.log(res)
+    //   }
+    // })
+    // //2.get请求，但是携带参数
+    // wx.request({
+    //   url: 'https://api.qingcigame.com/web/lantern/pet/info?limit=0',
+    //   data: {
+    //     type: 'sell',
+    //     page: 1
+    //   },
+    //   success: function (res) {
+    //     console.log(res)
+    //   }
+    // })
+    // //3.post 请求，并且携带参数
+    // wx.request({
+    //   url: 'http://httpbin.org/post',
+    //   method: 'post',
+    //   data: {
+    //     name: 'ccc',
+    //     age: 18
+    //   },
+    //   success: function (res) {
+    //     console.log(res)
+    //   },
+    //   fail: function (err) {
+    //     console.log(err)
+    //   }
+    // })
   }
+  
 })
